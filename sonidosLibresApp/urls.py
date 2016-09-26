aa780a31f84954940f10a9f01f492252b85f1f1d
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2016-09-25 20:11:11 -0500

    Error urls

--- sonidosLibresApp/urls.py
+++ sonidosLibresApp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^audios/(?P<pk>[0-9]+)/$', views.AudioDetail.as_view()),
     url(r'^categories/$', views.CategoryList.as_view()),
     url(r'^categories/(?P<pk>[0-9]+)/$', views.CategoryDetail.as_view()),
-    url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumList.as_view()),
+    url(r'^albums/$', views.AlbumList.as_view()),
 ]
 
 #urlpatterns =format_suffix_patterns(urlpatterns)
