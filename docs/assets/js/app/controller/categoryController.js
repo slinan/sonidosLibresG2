ac2bed49e0c4e830ed3fc394444ccf2e705bb0ab
akane <jarvein@hotmail.com>
2016-09-26 23:31:25 -0500

    include categoryController

---
+++ docs/assets/js/app/controller/categoryController.js
@@ -0,0 +1,28 @@
+mainApp.controller("categoryController", function($scope) {
+    $scope.categories = [
+        {
+            "id": 1,
+            "name": "Efectos de Sonido"
+        },
+        {
+            "id": 2,
+            "name": "Canciones"
+        },
+        {
+            "id": 3,
+            "name": "Experimental"
+        },
+        {
+            "id": 4,
+            "name": "Relajantes"
+        },
+        {
+            "id": 5,
+            "name": "Ambientales"
+        },
+        {
+            "id": 6,
+            "name": "Melodia"
+        }
+    ]
+});
